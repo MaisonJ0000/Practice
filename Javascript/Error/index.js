41b8501422565998b5f6427a57c233f2b5363346
MaisonJ0000 <maison.J0000@gmail.com>
2021-09-18 12:16:18 +0900

    bundlejs backup

--- Javascript/Error/index.js
+++ Javascript/Error/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-class BadRequestError extends Error();
+class BadRequestError extends Error {}
 
 app.listen(3000, () => {
   console.log('run');
