5dd35078df7b9a8b1589a24608a1b3257d8246fb
MaisonJ0000 <maisonj0000@gmail.com>
2021-12-05 09:54:41 +0900

    add algorithm skills. memo의 필요성 느낌2

--- Algorithm/Skills/index.js
+++ Algorithm/Skills/index.js
@@ -11,6 +11,14 @@ for (const [i, el] of arr.entries()) {
 }
 
 // push를 concat으로 변환해서 깔끔하던 예시도 있었는데, 아마 map to array일듯
+// 아래를 concat으로 하면 쉬웠던것같다다const map = {};
+for (let str of strings) {
+    const zSucc = getZSucc(str);
+    if (!map[zSucc]) map[zSucc] = [str];
+    else map[zSucc].push(str);
+}
+// return Object.values(map);
+
 
 
 
@@ -30,3 +38,20 @@ let cnt = 0;
 })();
 
 console.log(cnt);
+
+
+// sort 헷갈릴때
+
+const sorted = nums.sort((b2,b1) => b2-b1);
+
+
+const l = Array(s.length).fill(null).map(() => Array(s.length));
+
+
+//char
+const incCharNum = char.charCodeAt(0) + 1;
+// return String.fromCharCode(incCharNum);
+
+
+
+// splice slice 맨날 헷갈림, shift pop
